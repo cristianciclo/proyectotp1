98ac9d0e468af2f27df8c8a84a263eec7dbd498e
FranAlcon96 <franplayer96@gmail.com>
2017-03-09 20:26:11 +0100

    Add files via upload

---
+++ src/test/java/piladealmacen/Test1.java
@@ -0,0 +1,17 @@
+package piladealmacen;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import piladealmacen.*;
+public class Test1 {
+
+	@Test
+	public void test() {
+		Pale p = new Pale("0001",20.5,10);
+		assertEquals(p.getCodigo(),"0001");
+		assertEquals(p.getPeso(),20.5);
+		assertEquals(p.getNumeroCajas(),10);
+		fail("Not yet implemented");
+	}
+
+}
